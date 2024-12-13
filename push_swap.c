8e9198fc9ed721afb6dd3220471695e42bed3f53
Philippe Menard <phil.menard1668@gmail.com>
2024-12-13 16:33:01 +0100

    added four

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: pmenard <pmenard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 10:54:00 by pmenard           #+#    #+#             */
-/*   Updated: 2024/12/13 15:02:52 by pmenard          ###   ########.fr       */
+/*   Updated: 2024/12/13 16:31:23 by pmenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,45 +35,52 @@
 	print_list(*b, "b");
 } */
 
-void	triotri(t_list **a)
+void	sort_three(t_list **a, t_list **b)
 {
-	t_list	*first;
-	t_list	*second;
-	t_list	*third;
+	t_list	*a_first;
 
-	first = *a;
-	second = first->next;
-	third = second->next;
-	if (*(int *)third->content < *(int *)second->content 
-	&& *(int *)third->content > *(int *)first->content)
+	*a = ft_push(*a, b, "b");
+	a_first = *a;
+	if (*(int *)a_first->content > *(int *)a_first->next->content)
+		*a = ft_swap(*a, "a");
+	*b = ft_push(*b, a, "a");
+	a_first = *a;
+	if (*(int *)a_first->content > *(int *)a_first->next->content)
+	{
+		if (*(int *)a_first->content > *(int *)a_first->next->next->content)
+			*a = ft_rotate(a, *a, "a");
+		else
+			*a = ft_swap(*a, "a");
+	}
+}
+
+void	sort_four(t_list **a, t_list **b)
+{
+	t_list	*a_first;
+	t_list	*a_last;
+
+	*a = ft_push(*a, b, "b");
+	sort_three(a, b);
+	*b = ft_push(*b, a, "b");
+	a_last = *a;
+	while (a_last->next != NULL)
+		a_last = a_last->next;
+	a_first = *a;
+	if (*(int *)a_first->content > *(int *)a_first->next->content)
 	{
 		*a = ft_swap(*a, "a");
-		*a = ft_rotate(a, *a, "a");
+		sort_four(a, b);
 	}
-	else if (*(int *)first->content < *(int *)second->content 
-	&& *(int *)first->content > *(int *)third->content)
-		*a = ft_reverse(a, *a, "a");
-	else if (*(int *)first->content > *(int *)second->content 
-	&& *(int *)first->content < *(int *)third->content)
-		*a = ft_swap(*a, "a");
-	else if (*(int *)third->content < *(int *)first->content 
-	&& *(int *)third->content > *(int *)second->content)
+	else if (*(int *)a_first->content > *(int *)a_last->content)
 		*a = ft_rotate(a, *a, "a");
-	else if (*(int *)second->content < *(int *)first->content 
-	&& *(int *)second->content > *(int *)third->content)
-	{
-		*a = ft_rotate(a, *a, "a");
-		*a = ft_swap(*a, "a");
-	}
-	print_list(*a, "a");
 }
 
 int	main(int argc, char **argv)
 {
 	t_list	*a;
-	//t_list	*b;
+	t_list	*b;
 
-	//b = NULL;
+	b = NULL;
 	if (argc == 1)
 		return (printf("Error\n"), (1));
 	else
@@ -83,9 +90,10 @@ int	main(int argc, char **argv)
 		a = create_list(argv);
 		if (a == NULL)
 			return (printf("Error\n"), (1));
-		triotri(&a);
+		sort_four(&a, &b);
+		print_list(a, "a");
 		ft_lstclear(&a, &del);
-		//ft_lstclear(&b, &del);
+		ft_lstclear(&b, &del);
 	}
 	return (0);
 }
